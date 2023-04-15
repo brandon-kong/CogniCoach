d0e970a0f5656b8d70534cae6852d97ac59b75d4
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2023-04-15 15:49:24 -0500

    Update index.js

---
+++ app/screens/quiz/index.js
@@ -0,0 +1,16 @@
+export default function Quiz(){
+    return (
+        <View style={styles.container}>
+            <Text>Quiz</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+})
