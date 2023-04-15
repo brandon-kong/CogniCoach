62a777a9cbed03a5706899570a5cc8e7b9992524
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2023-04-15 15:43:21 -0500

    Update App.js

--- app/App.js
+++ app/App.js
@@ -1,18 +1,29 @@
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-// } from "react-router-dom"
-
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Quiz from "./Screens/Quiz";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom"
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path = "/"
+          // element = {<Home/>}
+        />
+        <Route
+          exact
+          path = "/Screens/Quiz"
+          element = {<Quiz/>}
+        />
+
+      </Routes>
+    </Router>
   );
 }
 
