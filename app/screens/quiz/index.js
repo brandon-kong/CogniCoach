c23063738dbad7bce1b7e72cdc714ab265994bdd
carolliu2024 <CarolLiu2024@u.northwestern.edu>
2023-04-15 15:55:09 -0500

    form

--- app/screens/quiz/index.js
+++ app/screens/quiz/index.js
@@ -1,8 +1,22 @@
+import React, { useState } from 'react';
+import { View, TextInput, TouchableOpacity, Text } from 'react-native';
+
 export default function Quiz(){
+    const [text, setText] = useState('');
+
     return (
         <View style={styles.container}>
             <Text>Quiz</Text>
-        </View>
+            <TextInput
+                style={{ height: 40, width: 200, borderColor: 'gray', borderWidth: 1 }}
+                onChangeText={text => setText(text)}
+                value={text}
+                placeholder="Put some text"
+            />
+            <TouchableOpacity onPress={handleSubmit}>
+                <Text>Submit</Text>
+            </TouchableOpacity>
+        </View>    
     );
 }
 
